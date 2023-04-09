f9af4050ff6d4ee6b960d4b614dbe52d7f1acde1
DKRusty <ltybc199516@yandex.ru>
2023-04-09 22:12:01 +0300

    First commit

---
+++ dbo.TR_Basket_insert_update.sql
@@ -0,0 +1,34 @@
+CREATE TRIGGER dbo.TR_Basket_insert_update
+ON dbo.basket  
+AFTER INSERT   
+AS 
+if @@ROWCOUNT>=2 
+begin
+		update dbo.Basket 
+		set DiscountValue=b.value*0.95 from  dbo.Basket b,inserted i
+		where b.id_sku in (select distinct i.id_sku from inserted i
+		group by i.id_sku
+		having count(i.id_sku)>=2) 
+		and b.value=i.value 
+		and b. DiscountValue=i. DiscountValue 
+		and i.id_sku=b.id_sku
+		update dbo.Basket
+		set DiscountValue=b.value*0 from  dbo.Basket b,inserted i
+		where b.id_sku in (select distinct i.id_sku from inserted i
+		group by i.id_sku 
+		having count(i.id_sku)<2) 
+		and b.value=i.value 
+		and b. DiscountValue=i. DiscountValue 
+		and i.id_sku=b.id_sku
+	end
+else 
+	begin
+		update dbo.Basket
+		set DiscountValue=b.value*0 from  dbo.Basket b,inserted i
+		where b.id_sku in (select distinct i.id_sku from inserted i
+		group by i.id_sku
+		having count(id_sku)<2) 
+		and b.value=i.value 
+		and b. DiscountValue=i.DiscountValue 
+		and i.id_sku=b.id_sku
+end;
