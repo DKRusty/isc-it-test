c5d97a6ca2c933e2927345128c8182feb145c6f5
DKRusty <ltybc199516@yandex.ru>
2023-04-11 15:33:10 +0300

    commit

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -13,7 +13,7 @@ begin
 		and b. DiscountValue=i. DiscountValue 
 		and i.id_sku=b.id_sku
 		update dbo.Basket
-		set DiscountValue=b.value*0 from  dbo.Basket b,inserted i
+		set DiscountValue=0 from  dbo.Basket b,inserted i
 		where b.id_sku in (select distinct i.id_sku from inserted i
 		group by i.id_sku 
 		having count(i.id_sku)<2) 
@@ -24,7 +24,7 @@ begin
 else 
 	begin
 		update dbo.Basket
-		set DiscountValue=b.value*0 from  dbo.Basket b,inserted i
+		set DiscountValue=0 from  dbo.Basket b,inserted i
 		where b.id_sku in (select distinct i.id_sku from inserted i
 		group by i.id_sku
 		having count(id_sku)<2) 
