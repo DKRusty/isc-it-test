8ea6759e0dcc8a68e52d26795c2693998512b764
DKRusty <ltybc199516@yandex.ru>
2023-04-11 22:22:08 +0300

    only one update v2

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -1,16 +1,15 @@
 CREATE TRIGGER dbo.TR_Basket_insert_update
 ON dbo.basket  
 AFTER INSERT   
-as
+AS 
 with t1 (t_id,ct) 
 as (select i.id_sku,count(i.id_sku) from inserted i
 		group by i.id_sku)
 update dbo.Basket 
 set DiscountValue = case 
-	when @@ROWCOUNT>=2 and t1.ct>=2 then b.value*0.05
-	when @@ROWCOUNT>=2 and t1.ct<2 then b.value*0
-	when @@ROWCOUNT<2  then b.value*0
-	end from  dbo.Basket b,t1,inserted i 
+when @@ROWCOUNT>=2 and t1.ct>=2 then b.value*0.05
+else 0
+end from  dbo.Basket b,t1,inserted i 
 where t1.t_id=b.id_sku
 and b.value=i.value 
 and b.DiscountValue=i.DiscountValue 
