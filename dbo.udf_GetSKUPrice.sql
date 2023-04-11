373230c316b977016be225c00ffb485b5210f10b
DKRusty <ltybc199516@yandex.ru>
2023-04-11 19:19:13 +0300

    use ISNULL

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -3,10 +3,8 @@ RETURNS decimal(18, 2)
 AS
 BEGIN
     DECLARE @res decimal(18, 2);
-    SELECT @res = SUM(b.Quantity)/SUM(b.value)   
+    SELECT @res = isnull(SUM(b.Quantity)/SUM(b.value),0)   
     FROM dbo.Basket b
     WHERE b.ID_SKU = @ID_SKU;
-     IF (@res IS NULL)
-        SET @res = 0;
     RETURN @res;
 END;
